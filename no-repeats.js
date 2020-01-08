0ca73026d633f2472ba7241283f026cac5de9d14
shanelonergan <sptlonergan@gmail.com>
2020-01-07 18:25:02 -0700

    added some examples

--- no-repeats.js
+++ no-repeats.js
@@ -2,3 +2,16 @@
 
 // For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
 
+// examples
+
+// abcd permutations = [abcd, bacd, abdc, badc, cdab, dcab, cdba, dcba], and all are non repeating. return 6
+
+// INDECIES: 1234 permutations = [1234, 1243, 1324, 1342, 1423, 1432, 2134, 2143, 2314, 2341, 2413, 2431, 3124, 3142, 3214, 3241, 3412, 3421, 4123, 4132, 4213, 4231, 4312, 4321]
+// aabb permutations = [aabb, aabb, bbaa, bbaa, abab, abab]
+
+
+function permAlone(str) {
+  return str;
+}
+
+permAlone('aab');
