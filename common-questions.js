049f66eab6765a9276df7e25a09a77b4c7cf9c61
shanelonergan <sptlonergan@gmail.com>
2020-01-13 22:11:52 -0500

    cleaner soltion

--- common-questions.js
+++ common-questions.js
@@ -2,14 +2,34 @@
 
 // print the numbers from 1-100. if a number is devisable by 3, instead print fizz. if it is devisable by 5, instead print buzz.
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz')
-    } else if (i % 3 === 0) {
-        console.log('Fizz')
-    } else if (i % 5 === 0) {
-        console.log('Buzz')
-    } else {
-        console.log(i)
+let solution1 = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz')
+        } else if (i % 3 === 0) {
+            console.log('Fizz')
+        } else if (i % 5 === 0) {
+            console.log('Buzz')
+        } else {
+            console.log(i)
+        }
     }
 }
+
+let solution2 = () => {
+    for (let i = 0; i < .100; i++) {
+
+        let output
+
+        switch (true) {
+            case (i % 3 === 0 && i % 5 === 0):
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+solution1()
