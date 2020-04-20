f3eee2445bb1044199aca1bf8465533574ab47a2
shanelonergan <sptlonergan@gmail.com>
2020-04-20 10:50:20 -0400

    hackerrank soln/

--- hackerrank.js
+++ hackerrank.js
@@ -5,8 +5,35 @@
 // For example, if the string s = 'abcac' and n = 10, the substring we consider is abcacabcac, the first 10 characters of her infinite string. There are 4 occurrences of a in the substring.
 
 function repeatedString(str, num) {
-    let strArr = str.split('')
-    if(str.length === 1 || str.every(val => arr[0])) {
-        return num
-    }
+	const strArr = str.split('')
+
+	if (!strArr.includes("a")) {
+        return 0
+    } else if (str.length === 1 || strArr.every((val) => val === strArr[0])) {
+		return num
+	}
+
+	const numOfAs = strArr.reduce((acc, letter) => {
+		letter === 'a' ? acc++ : null
+		return acc
+	}, 0)
+
+	const part1 = Math.floor(num / str.length) * numOfAs
+	const part2 = () => {
+		const remainder = num % str.length
+		let extra = 0
+		if (remainder) {
+			for (let i = 0; i < remainder; i++) {
+				if (strArr[i] === 'a') {
+					extra++
+				}
+			}
+		}
+		return extra
+	}
+
+	console.log(part1, part2(), part1 + part2())
+	return part1 + part2()
 }
+
+repeatedString('x', 10000000000)
